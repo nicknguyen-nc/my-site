d9d6ef26c75eae427cbd492920cffbc2fc412462
Kathoey <nicholasnguyen75@gmail.com>
2023-05-01 17:35:16 -0400

    changed navbar colors

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,19 +4,19 @@ import "./App.css"
 
 function Navbar() {
     return (
-        <div class="bg-sky-900 flex flex-row justify-center p-4 text-amber-100">
+        <div class="bg-blue-950 flex flex-row justify-center p-4 text-amber-100">
             <Link class=""to="/">
-                <div class="flex-shrink-0 px-6 hover:bg-cyan-600 rounded-lg p-2">
+                <div class="flex-shrink-0 px-6 hover:bg-cyan-800 rounded-lg p-2">
                     Home
                 </div>
             </Link>
             <Link to="/blogs">
-                <div class="flex-shrink-0 px-6 hover:bg-cyan-600 rounded-lg p-2">
+                <div class="flex-shrink-0 px-6 hover:bg-cyan-800 rounded-lg p-2">
                     Blogs
                 </div>
             </Link>
             <Link to="/contact">
-                <div class="flex-shrink-0 px-6 hover:bg-cyan-600 rounded-lg p-2">
+                <div class="flex-shrink-0 px-6 hover:bg-cyan-800 rounded-lg p-2">
                     Contact
                 </div>
             </Link>
